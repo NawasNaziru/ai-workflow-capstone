8bd4e5cb0a0fee0e8a1bff24469acacc66a53b4b
NawasNaziru <nawasnaziru@gmail.com>
2020-12-13 12:23:25 +0100

    Update GaussianRegressorModel.py

--- GaussianRegressorModel.py
+++ GaussianRegressorModel.py
@@ -1,19 +1,210 @@
 import numpy as np
+import time,os,re,csv,sys,uuid,joblib
+from datetime import date
+from collections import defaultdict
 
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.pipeline import Pipeline
 
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
 
-# Create an instance of a Gaussian Process model
+from logger import update_predict_log, update_train_log
+from cslib import fetch_ts, engineer_features
 
-kernel = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2))
+## model specific variables (iterate the version and note with each change)
+MODEL_DIR = "models"
+MODEL_VERSION = 0.1
+MODEL_VERSION_NOTE = "supervised learing model for time-series"
 
-gpr = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=9)
+def _model_train(df,tag,test=False):
+ 
 
-# Fit the AAVAIL training data
 
-gpr.fit(X, y)
 
-# Make the prediction on the test data
+    ## start timer for runtime
+    time_start = time.time()
+    
+    X,y,dates = engineer_features(df)
 
-y_predicted, sigma = gpr.predict(x, return_std=True)
+    if test:
+        n_samples = int(np.round(0.3 * X.shape[0]))
+        subset_indices = np.random.choice(np.arange(X.shape[0]),n_samples,
+                                          replace=False).astype(int)
+        mask = np.in1d(np.arange(y.size),subset_indices)
+        y=y[mask]
+        X=X[mask]
+        dates=dates[mask]
+        
+    ## Perform a train-test split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+    # Create an instance of a Gaussian Process model
+
+    kernel = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2))
+
+    gpr = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=9)
+
+    # Fit the AAVAIL training data
+
+    gpr.fit(X_train, y_train)
+
+    y_pred = gpr.predict(X_test)
+    eval_rmse =  round(np.sqrt(mean_squared_error(y_test,y_pred)))
+    
+    ## retrain using all data
+    gpr.fit(X, y)
+    model_name = re.sub("\.","_",str(MODEL_VERSION))
+    if test:
+        saved_model = os.path.join(MODEL_DIR,
+                                   "test-{}-{}.joblib".format(tag,model_name))
+        print("... saving test version of model: {}".format(saved_model))
+    else:
+        saved_model = os.path.join(MODEL_DIR,
+                                   "sl-{}-{}.joblib".format(tag,model_name))
+        print("... saving model: {}".format(saved_model))
+        
+    joblib.dump(gpr,saved_model)
+
+    m, s = divmod(time.time()-time_start, 60)
+    h, m = divmod(m, 60)
+    runtime = "%03d:%02d:%02d"%(h, m, s)
+
+    ## update log
+    update_train_log(tag,(str(dates[0]),str(dates[-1])),{'rmse':eval_rmse},runtime,
+                     MODEL_VERSION, MODEL_VERSION_NOTE,test=True) """
+  
+
+
+
+def model_train(data_path):
+
+    train_data = fetch_ts(data_path)
+
+
+
+    for country,df in train_data.items():
+
+        
+
+        
+
+
+        _model_train(df,country,test=test)
+        
+        
+def model_load(prefix='test',data_path=None,training=True):
+    """
+    example funtion to load model
+    
+    The prefix allows the loading of different models
+    """
+
+    if not data_path:
+        data_path = os.path.join("data","cs-train")
+    
+    models = [f for f in os.listdir(os.path.join(".","models")) if re.search("test",f)]
+
+    if len(models) == 0:
+        raise Exception("Models with prefix '{}' cannot be found did you train?".format(prefix))
+
+    all_models = {}
+    for model in models:
+        all_models[re.split("-",model)[1]] = joblib.load(os.path.join(".","models",model))
+
+    ## load data
+    train_data = fetch_ts(data_path)
+    all_data = {}
+    for country, df in train_data.items():
+        X,y,dates = engineer_features(df,training=training)
+        dates = np.array([str(d) for d in dates])
+        all_data[country] = {"X":X,"y":y,"dates": dates}
+        
+    return(all_data, all_models)
+
+def model_predict(country,year,month,day,all_models=None,test=False):
+    """
+    example funtion to predict from model
+    """
+
+    ## start timer for runtime
+    time_start = time.time()
+
+    ## load model if needed
+    if not all_models:
+        all_data,all_models = model_load(training=False)
+    
+    ## input checks
+    if country not in all_models.keys():
+        raise Exception("ERROR (model_predict) - model for country '{}' could not be found".format(country))
+
+    for d in [year,month,day]:
+        if re.search("\D",d):
+            raise Exception("ERROR (model_predict) - invalid year, month or day")
+    
+    ## load data
+    model = all_models[country]
+    data = all_data[country]
+
+    ## check date
+    target_date = "{}-{}-{}".format(year,str(month).zfill(2),str(day).zfill(2))
+    print(target_date)
+
+    if target_date not in data['dates']:
+        raise Exception("ERROR (model_predict) - date {} not in range {}-{}".format(target_date,
+                                                                                    data['dates'][0],
+                                                                                    data['dates'][-1]))
+    date_indx = np.where(data['dates'] == target_date)[0][0]
+    query = data['X'].iloc[[date_indx]]
+    
+    ## sainty check
+    if data['dates'].shape[0] != data['X'].shape[0]:
+        raise Exception("ERROR (model_predict) - dimensions mismatch")
+
+    ## make prediction and gather data for log entry
+    y_pred = model.predict(query)
+    y_proba = None
+    if 'predict_proba' in dir(model) and 'probability' in dir(model):
+        if model.probability == True:
+            y_proba = model.predict_proba(query)
+
+
+    m, s = divmod(time.time()-time_start, 60)
+    h, m = divmod(m, 60)
+    runtime = "%03d:%02d:%02d"%(h, m, s)
+
+    ## update predict log
+    """ update_predict_log(country,y_pred,y_proba,target_date,
+                       runtime, MODEL_VERSION, test=test) """
+    
+    return({'y_pred':y_pred,'y_proba':y_proba})
+
+if __name__ == "__main__":
+
+    """
+    basic test procedure for GaussianRegressorModel.py
+    """
+
+    ## train the model
+    print("TRAINING MODELS")
+    data_dir = os.path.join("data","cs_train")
+    model_train(data_path,test=True)
+
+    ## load the model
+    print("LOADING MODELS")
+    all_data, all_models = model_load()
+    print("... models loaded: ",",".join(all_models.keys()))
+
+    ## test predict
+    country='all'
+    year='2018'
+    month='01'
+    day='05'
+    result = model_predict(country,year,month,day)
+    print(result)
+
+
